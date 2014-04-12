2b0b8ce75997691a8c5a38eacc9ea179311253e6
dabyv <fazel.r.k@gmail.com>
2014-04-12 19:28:51 +0430

    injor chiza hale adamo migire

--- sql/updates/characters/passive ac.sql
+++ sql/updates/characters/passive ac.sql
@@ -15,16 +15,16 @@ CREATE TABLE `players_reports_status` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='';
 
 DROP TABLE IF EXISTS `daily_players_reports`;
-CREATE TABLE `daily_players_reports` (
-  `guid` int(10) unsigned NOT NULL DEFAULT '0',
-  `creation_time` int(10) unsigned NOT NULL DEFAULT '0',
-  `average` float NOT NULL DEFAULT '0',
-  `total_reports` bigint(20) unsigned NOT NULL DEFAULT '0',
-  `speed_reports` bigint(20) unsigned NOT NULL DEFAULT '0',+
-  `fly_reports` bigint(20) unsigned NOT NULL DEFAULT '0',
-  `jump_reports` bigint(20) unsigned NOT NULL DEFAULT '0',
-  `waterwalk_reports` bigint(20) unsigned NOT NULL DEFAULT '0',
-  `teleportplane_reports` bigint(20) unsigned NOT NULL DEFAULT '0',
-  `climb_reports` bigint(20) unsigned NOT NULL DEFAULT '0',
-  PRIMARY KEY (`guid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='';
+CREATE TABLE `daily_players_reports`(
+ `guid` INT(10) UNSIGNED NOT NULL DEFAULT 0,
+ `creation_time` INT(10) UNSIGNED NOT NULL DEFAULT 0,
+ `average` FLOAT NOT NULL DEFAULT 0,
+ `total_reports` BIGINT(20) UNSIGNED NOT NULL DEFAULT 0,
+ `speed_reports` BIGINT(20) UNSIGNED NOT NULL DEFAULT 0,
+ `fly_reports` BIGINT(20) UNSIGNED NOT NULL DEFAULT 0,
+ `jump_reports` BIGINT(20) UNSIGNED NOT NULL DEFAULT 0,
+ `waterwalk_reports` BIGINT(20) UNSIGNED NOT NULL DEFAULT 0,
+ `teleportplane_reports` BIGINT(20) UNSIGNED NOT NULL DEFAULT 0,
+ `climb_reports` BIGINT(20) UNSIGNED NOT NULL DEFAULT 0,
+ PRIMARY KEY (`guid`)
+) ENGINE=INNODB CHARSET=utf8; 
